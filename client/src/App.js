f7f69109e9dedf88ba6e8c1c8f4e909b1b08a068
KIM-94 <ksw2102@gmail.com>
2021-01-01 02:19:58 +0000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -15,18 +15,18 @@ import { withStyles } from '@material-ui/core/styles';
 
 
 const styles = theme => ({
-    root: {
-        width: "100%",
-        marginTop: theme.spacing.unit * 3,
-        overflowX: "auto",
-    },
-    table: {
-        minWidth: 1080/2
-    },
-    progress: {
-        matgin: theme.spacing.unit * 2 
-    }
-    
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080 / 2
+  },
+  progress: {
+    matgin: theme.spacing.unit * 2
+  }
+
 });
 
 // const customer = [
@@ -58,66 +58,66 @@ const styles = theme => ({
 
 
 class App extends Component {
-    
-    state = {
-        customer: "",
-        completed: 0
-    }
 
-    componentDidMount(){
-        this.timer = setInterval(this.progress, 20); 
-        this.callApi()
-        .then(res => this.setState({customer: res}))
-        .catch(err => console.log(err));
-    }
+  state = {
+    customer: "",
+    completed: 0
+  }
 
-    callApi = async() => {
-        const response = await fetch('/api/customers');
-        const body = await response.json();
-        return body;
-    }
-    
-    progress = () => {
-        const { completed } = this.state;
-        this.setState({ completed: completed >= 100 ? 0 : completed + 2 });
-    }
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({ customer: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 2 });
+  }
 
   render() {
-      const { classes } = this.props;
+    const { classes } = this.props;
     return (
-        <Paper className={classes.root}>
-          <Table className={classes.table}>
-            <TableHead>
-                <TableRow>
-                    <TableCell>번호</TableCell>
-                    <TableCell>이미지</TableCell>
-                    <TableCell>이름</TableCell>
-                    <TableCell>생년월일</TableCell>
-                    <TableCell>성별</TableCell>
-                    <TableCell>직업</TableCell>
-                </TableRow>
-            </TableHead>    
-            <TableBody>
-               { this.state.customer ? this.state.customer.map(c => {
-                    return (
-                      <Customer
-                        key={c.id}
-                        id={c.id}
-                        image={c.image}
-                        name={c.name}
-                        birthday={c.birthday}
-                        gender={c.gender}
-                        job={c.job}
-                      />
-                    );
-                  }) : 
-                   <TableRow>
-                    <TableCell colSpan="6" align="center">
-                        <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                    </TableCell>
-                </TableRow>
-               }
-            </TableBody>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customer ? this.state.customer.map(c => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.NAME}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+            }
+          </TableBody>
         </Table>
       </Paper>
     );
